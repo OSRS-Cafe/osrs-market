c26bb9b4553ad553f47a33396edc7d325f2f4f24
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2026-05-02 12:15:27 +0200

    Ores

---
+++ src/data/lists/ore-list.ts
@@ -0,0 +1,15 @@
+import {ItemID} from "../item-ids.ts";
+
+export const ore_list = [
+	ItemID.COPPER_ORE,
+	ItemID.TIN_ORE,
+	ItemID.IRON_ORE,
+	ItemID.SILVER_ORE,
+	ItemID.LEAD_ORE,
+	ItemID.COAL,
+	ItemID.GOLD_ORE,
+	ItemID.MITHRIL_ORE,
+	ItemID.ADAMANTITE_ORE,
+	ItemID.NICKEL_ORE,
+	ItemID.RUNITE_ORE,
+];
--- src/views/ListsView.tsx
+++ src/views/ListsView.tsx
@@ -1,18 +1,19 @@
 import styles from "./ListsView.module.css";
-import {useTlStore} from "../zustand/tlStore.ts";
 import {rune_list} from "../data/lists/rune-list.ts";
 import ItemDisplaySquare from "../components/ItemDisplaySquare";
-import * as React from "react";
+import {ore_list} from "../data/lists/ore-list.ts";
 
 function ListsView() {
-	const tl = useTlStore((s) => s.strings);
-
 
 	return (
 		<div className={styles.root}>
 			<h3>Lists</h3>
 			<h4>Runes</h4>
 			<div className={styles.listContainer}>{rune_list.map((rune_id) => (
+				<ItemDisplaySquare id={rune_id}/>
+			))}</div>
+			<h4>Ores</h4>
+			<div className={styles.listContainer}>{ore_list.map((rune_id) => (
 				<ItemDisplaySquare id={rune_id}/>
 			))}</div>
 		</div>
